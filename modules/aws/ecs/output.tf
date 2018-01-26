541cb12c859486ccda35a4383e73e271a3237fd5
Hugo Almeida <hugo.almeida@boldlink.io>
2018-01-26 12:34:48 +0000

    Fixing ecs ouput format

--- modules/aws/ecs/output.tf
+++ modules/aws/ecs/output.tf
@@ -1,7 +1,7 @@
 output "ecs_id" {
-  value = {aws_ecs_cluster.mod.id}
+  value = "${aws_ecs_cluster.mod.id}"
 }
 
 output "ecs_arn" {
-  value = {aws_ecs_cluster.mod.arn}
+  value = "${aws_ecs_cluster.mod.arn}"
 }
