987854712c6dc4acb038589cd27494d1a0f8378c
hugo.almeida <hugo.almeida@zpg.co.uk>
2019-01-15 17:36:32 +0000

    NOSTORY: Change userdata

--- modules/aws/ecs/clusterEc2/data.tf
+++ modules/aws/ecs/clusterEc2/data.tf
@@ -116,63 +116,6 @@ ECS_INSTANCE_ATTRIBUTES={"workload": "$${workload}"}
 ECS_CONTAINER_INSTANCE_TAGS={"owner": "$${owner}","workload": "$${workload}"}
 EOC
 
---==BOUNDARY==
-Content-Type: text/x-shellscript; charset="us-ascii"
-#!/bin/bash
-# Inject the CloudWatch Logs configuration file contents
-cat > /etc/awslogs/awslogs.conf <<- EOC
-[general]
-state_file = /var/lib/awslogs/agent-state
-
-[/var/log/dmesg]
-file = /var/log/dmesg
-log_group_name = /infrastructure/ecs/$${clustername}
-log_stream_name = $${groupname}/{container_instance_id}/dmesg
-
-[/var/log/messages]
-file = /var/log/messages
-log_group_name = /infrastructure/ecs/$${clustername}
-log_stream_name = $${groupname}/{container_instance_id}/messages
-datetime_format = %b %d %H:%M:%S
-
-[/var/log/docker]
-file = /var/log/docker
-log_group_name = /infrastructure/ecs/$${clustername}
-log_stream_name = $${groupname}/{container_instance_id}/docker
-datetime_format = %Y-%m-%dT%H:%M:%S.%f
-
-[/var/log/ecs/ecs-init.log]
-file = /var/log/ecs/ecs-init.log
-log_group_name = /infrastructure/ecs/$${clustername}
-log_stream_name = $${groupname}/{container_instance_id}/ecs-init
-datetime_format = %Y-%m-%dT%H:%M:%SZ
-
-[/var/log/ecs/ecs-agent.log]
-file = /var/log/ecs/ecs-agent.log.*
-log_group_name = /infrastructure/ecs/$${clustername}
-log_stream_name = $${groupname}/{container_instance_id}/ecs-agent
-datetime_format = %Y-%m-%dT%H:%M:%SZ
-
-[/var/log/ecs/audit.log]
-file = /var/log/ecs/audit.log.*
-log_group_name = /infrastructure/ecs/$${clustername}
-log_stream_name = $${groupname}/{container_instance_id}/audit
-datetime_format = %Y-%m-%dT%H:%M:%SZ
-
-EOC
-
-# Set the region to send CloudWatch Logs data to (the region where the container instance is located)
-region=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
-sed -i -e "s/region = us-east-1/region = $region/g" /etc/awslogs/awscli.conf
-
-# Configure and start CloudWatch Logs agent on Amazon ECS container instance
-# Grab the container instance ARN from instance metadata
-container_instance_id=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r .instanceId)
-# Replace the container instance ID placeholders with the actual values
-sed -i -e "s/{container_instance_id}/$container_instance_id/g" /etc/awslogs/awslogs.conf
-systemctl enable awslogsd
-systemctl start awslogsd
---==BOUNDARY==--
 EOF
 
 }
