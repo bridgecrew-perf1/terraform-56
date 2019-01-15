8ee4c338fe3bdfa8915d37d056cc2a8ea2d33a17
hugo.almeida <hugo.almeida@zpg.co.uk>
2019-01-15 17:37:02 +0000

    NOSTORY: Adding Policy Module first commit

---
+++ modules/aws/autoscaling/ec2/policytt/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version  = "> 0.11.6"
+}
+
+resource "aws_autoscaling_policy" "main" {
+  name                            = "${var.name}"
+  autoscaling_group_name          = "${var.autoscaling_group_name}"
+  policy_type                     = "${var.policy_type}"
+  estimated_instance_warmup       = "${var.estimated_instance_warmup}"
+  target_tracking_configuration {
+    customized_metric_specification {
+      metric_dimension {
+        name                      = "${var.metric_dimension_name}"
+        value                     = "${var.metric_dimension_value}"
+      }
+      metric_name                 = "${var.metric_name}"
+      namespace                   = "${var.namespace}"
+      statistic                   = "${var.statistic}"
+    }
+    target_value                  = "${var.target_value}"
+  } 
+}
---
+++ modules/aws/autoscaling/ec2/policytt/output.tf
@@ -0,0 +1,16 @@
+output "arn" {
+  value = "${aws_autoscaling_policy.main.arn}"
+}
+
+output "name" {
+  value = "${aws_autoscaling_policy.main.name}"
+}
+
+output "autoscaling_group_name" {
+  value = "${aws_autoscaling_policy.main.autoscaling_group_name}"
+}
+
+output "policy_type" {
+  value = "${aws_autoscaling_policy.main.policy_type}"
+}
+
