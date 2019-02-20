0fdf60e0738bcdd553046f892e49af3b9cc33993
hugo.almeida <hugo.almeida@zpg.co.uk>
2019-02-20 12:37:05 +0000

    NOSTORY: Fix tgw module

--- modules/aws/vpc/tgw/main.tf
+++ modules/aws/vpc/tgw/main.tf
@@ -182,19 +182,3 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "main" {
   }
 }
 
-resource "aws_ec2_transit_gateway_route" "main" {
-  destination_cidr_block         = "${var.cidr}"
-  transit_gateway_attachment_id  = "${aws_ec2_transit_gateway_vpc_attachment.main.id}"
-  transit_gateway_route_table_id = "${aws_ec2_transit_gateway.main.association_default_route_table_id}"
-}
-
-resource "aws_ec2_transit_gateway_route_table_association" "main" {
-  transit_gateway_attachment_id  = "${aws_ec2_transit_gateway_vpc_attachment.main.id}"
-  transit_gateway_route_table_id = "${aws_ec2_transit_gateway.main.association_default_route_table_id}"
-}
-
-resource "aws_ec2_transit_gateway_route_table_propagation" "example" {
-  transit_gateway_attachment_id  = "${aws_ec2_transit_gateway_vpc_attachment.main.id}"
-  transit_gateway_route_table_id = "${aws_ec2_transit_gateway.main.association_default_route_table_id}"
-}
-
